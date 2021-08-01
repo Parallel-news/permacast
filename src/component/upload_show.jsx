a16c46b3c2650e617ab4833a064d6ec1f63b4a49
xylophonezygote <xylophonezygote@gmail.com>
2021-08-01 13:44:07 +0300

    upload_show component for adding a new show from the top bar

--- src/component/upload_show.jsx
+++ src/component/upload_show.jsx
@@ -1,13 +1,67 @@
 import { React, useState } from 'react';
-import { Button, Modal } from 'react-bootstrap';
+import { Button, Modal, Form } from 'react-bootstrap';
+import Arweave from 'arweave'
+
+const arweave = Arweave.init({
+  host: "arweave.net",
+  port: 443,
+  protocol: "https",
+  timeout: 100000,
+  logging: false,
+});
+
 
 export default function PodcastHtml()  {
 
     const [show, setShow] = useState(false);
 
-    const handleUpload = () => {
+    const handleUploadClick = () => {
         setShow(true);
       };
+
+    const readFile = async (e) => {
+      //e.preventDefault()
+      const reader = new FileReader()
+      reader.onload = async (e) => { 
+        const file = (e.target.result)
+        return file
+      };
+      reader.readAsArrayBuffer(e.target.files[0])
+    }
+
+     const uploadToArweave = (file) => {
+      const wallet = JSON.parse(sessionStorage.getItem("arweaveWallet"));
+      const data = readFile(file)
+      console.log(data)
+      if (!wallet) { return null } else {
+        arweave.createTransaction({ data: data }, wallet).then((tx) => {
+          tx.addTag("Content-Type", data.type);
+          arweave.transactions.sign(tx, wallet).then(() => {
+            arweave.transactions.post(tx, wallet).then((response) => {
+              console.log(response)
+              if (response.statusText === "OK") {
+                console.log(response.statusText)
+              }
+            });
+          });
+        });
+      }
+    }
+
+    const handleShowUpload = (event) => {
+      event.preventDefault()
+      const podcastName = event.target.podcastName.value
+      const podcastDescription = event.target.podcastDescription.value
+      const podcastCover = event.target.podcastCover.files[0]
+      console.log(podcastCover)
+      const podcastCoverTxId = uploadToArweave(podcastCover)
+      const showObj = {
+        "name" : podcastName,
+        "desc" : podcastDescription,
+        "cover" : podcastCoverTxId
+      }
+      console.log(showObj);
+    }
 
     const handleClose = () => {
         setShow(false);
@@ -16,7 +70,7 @@ export default function PodcastHtml()  {
     return(  
         <>  
         <span className="mr-2 ml-2">
-            <Button onClick={handleUpload}>Upload</Button>
+            <Button onClick={() => handleUploadClick()}>Upload</Button>
         </span> 
         <Modal
         show={show}
@@ -26,25 +80,39 @@ export default function PodcastHtml()  {
       >
         <Modal.Title className="p-4" id="alert-dialog-title">
           {"Add a new show"}
-          <br></br>
+          <br/>
+          <small className="font-small font-italics">{"You'll add episodes to the show next."}</small>
+          <br/>
         </Modal.Title>
         <Modal.Body className="m-2">
-        
+          <Form onSubmit={handleShowUpload}>
+            <Form.Group className="mb-3" controlId="podcastName">
+              <Form.Label>Show name</Form.Label>
+              <Form.Control type="text" name="podcastName" placeholder="The Arweave Show" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="podcastDescription">
+              <Form.Label>Show description</Form.Label>
+              <Form.Control as="textarea" name="podcastDescription" placeholder="This is a show about..." rows={3} />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="podcastCover" />
+              <Form.Label>Cover image</Form.Label>
+              <Form.Control type="file" onChange={(e) => readFile(e)} name="podcastCover"/>
         <br/><br/>
-        </Modal.Body>
         <Modal.Footer className="m-2">
         <Button variant="danger" onClick={handleClose} color="danger">
-            Cancel)
+            Cancel
           </Button>
         <Button
+              type="submit"
               variant="success"
               color="default"
               component="span"
             >
               Upload
             </Button>
-          
         </Modal.Footer>
+        </Form>
+        </Modal.Body>
       </Modal>
       </>
     )
