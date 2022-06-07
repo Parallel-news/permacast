c3b3af2f1d2b99b4d06c8f867a2d103b1c87d48e
Max Knivets <knivets.m@gmail.com>
2022-06-06 21:13:02 -0400

    Adds feed sorting feature

--- src/component/index.jsx
+++ src/component/index.jsx
@@ -2,12 +2,21 @@ import { React, useState, useEffect } from 'react'
 import PodcastHtml from './podcast_html.jsx'
 import { MESON_ENDPOINT } from '../utils/arweave.js'
 import { useTranslation } from 'react-i18next'
-import fetchPodcasts from '../utils/podcast.js'
+import { fetchPodcasts, sortPodcasts } from '../utils/podcast.js'
 
 export default function Index() {
   const [loading, setLoading] = useState(false)
   const [podcastsHtml, setPodcastsHtml] = useState([])
   const { t } = useTranslation()
+  const [sortedPodcasts, setSortedPodcasts] = useState()
+  const [counter, setCounter] = useState(0)
+  const filters = [
+    {type: "default", desc: t("sorting.default")},
+    {type: "podcastsactivity", desc: t("sorting.podcastsactivity")},
+    {type: "episodescount", desc: t("sorting.episodescount")}
+  ]
+  const filterTypes = filters.map(f => f.type)
+  const currentSelection = () => counter % filterTypes.length
 
   const renderPodcasts = (podcasts) => {
     let html = []
@@ -33,19 +42,35 @@ export default function Index() {
     const fetchData = async () => {
       setLoading(true)
       const podcasts = await fetchPodcasts()
+      const sorted = await sortPodcasts(filterTypes)
       const podcastsHtml = renderPodcasts(podcasts)
+      sorted['default'] = podcasts
       setPodcastsHtml(podcastsHtml)
+      setSortedPodcasts(sorted)
       setLoading(false)
     }
     fetchData()
   }, [])
+
+  useEffect(() => {
+    if (!counter) return
+    const filteredPodcasts = sortedPodcasts[filterTypes[currentSelection()]]
+    const newPodcasts = renderPodcasts(filteredPodcasts)
+    setPodcastsHtml(newPodcasts)  
+  }, [counter])
+
 
   return (
     <div>
       <div className="flex items-center justify-center p-2 md:p-6 text-md">
         {loading ? t("loading") : podcastsHtml.length === 0 ? t("nopodcasts") : null}
       </div>
-
+      <div 
+        className="btn btn-outline btn-secondary btn-sm md:btn-md text-sm md:text-md normal-case" 
+        onClick={() => setCounter(counter + 1)}
+      >
+        {t("sortpodcastsby") +": " + filters[currentSelection()].desc}
+      </div>
       <div className="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-3 lg:grid-cols-3 xl:gap-x-36 mb-10">
         {podcastsHtml}
       </div>
