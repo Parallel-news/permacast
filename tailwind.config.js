ec915752788e9b39b028b6e2778929be7b84ae94
Max Knivets <knivets.m@gmail.com>
2022-06-27 10:16:38 -0400

    Temporarily disables daisyUI due to build-errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,6 @@ module.exports = {
     require("@tailwindcss/aspect-ratio"),
     require("@tailwindcss/typography"),
     require("@tailwindcss/line-clamp"),
-    require("daisyui"),
+    // require("daisyui"), // causes a build error
   ],
 };
